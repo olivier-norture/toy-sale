104cad7beb9d4e600ddb7d62ee33f2e5e4d1a874
Olivier Norture <olivier@norture.fr>
2025-10-04 13:55:28 +0000

    [GEMINI] Feat: Move price to bottom right of label

--- web/print_label.php
+++ web/print_label.php
@@ -39,11 +39,9 @@ $description = $objet->getDescription();
 </head>
 <body>
     <div class="label-container">
-        <div class="label-ref-price">
-            <div><?php echo htmlspecialchars($objet->getRef()); ?></div>
-            <div><?php echo htmlspecialchars(number_format($objet->getPrix(), 2, ',', '') . '€'); ?></div>
-        </div>
+        <div class="label-ref"><?php echo htmlspecialchars($objet->getRef()); ?></div>
         <div class="label-description"><?php echo htmlspecialchars($description); ?></div>
+        <div class="label-price"><?php echo htmlspecialchars(number_format($objet->getPrix(), 2, ',', '') . '€'); ?></div>
     </div>
 </body>
 </html>
